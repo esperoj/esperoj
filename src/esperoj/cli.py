d1067f8bc73f1a1f391b9f96dae6a35295a2316c
esperoj <esperoj@esperoj.eu.org>
2023-12-17 13:52:57 +0000

    init

--- src/esperoj/cli.py
+++ src/esperoj/cli.py
@@ -1,16 +1,30 @@
 """Esperoj CLI."""
 
+import os
 from pathlib import Path
 
 import typer
 
-from esperoj.airtable import Airtable
 from esperoj.esperoj import Esperoj
-from esperoj.storj import Storj
 
 app = typer.Typer()
-db = Airtable()
-storage = Storj()
+
+match os.environ.get("ESPEROJ_DATABASE"):
+    case "Airtable":
+        from esperoj.airtable import Airtable
+
+        db: object = Airtable()
+    case _:
+        db = typer.Typer()
+
+match os.environ.get("ESPEROJ_STORAGE"):
+    case "Storj":
+        from esperoj.storj import Storj
+
+        storage: object = Storj()
+    case _:
+        storage = typer.Typer()
+
 esperoj = Esperoj(db=db, storage=storage)
 
 
---
+++ tests/test_cli.py
@@ -0,0 +1,9 @@
+"""Test Esperoj CLI."""
+
+from typer.testing import CliRunner
+
+runner = CliRunner()
+
+
+def test_echo():
+    assert 1
