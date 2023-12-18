0ebc6fcb4fef61388acaf23855b7557244ec54cd
esperoj <esperoj@esperoj.eu.org>
2023-12-18 01:55:14 +0000

    init

---
+++ src/esperoj/database.py
@@ -0,0 +1,9 @@
+"""Module contain Database class."""
+
+
+class Database:
+    def __init__(self):
+        pass
+
+    def table(self, name: str):
+        """Return the table."""
---
+++ src/esperoj/storage.py
@@ -0,0 +1,11 @@
+"""Module contain Storage class."""
+
+from pathlib import Path
+
+
+class Storage:
+    def __init__(self):
+        pass
+
+    def upload_file(self, path: Path, name: str):
+        """Upload the file."""
