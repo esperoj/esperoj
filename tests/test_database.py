2d067ea305dcebde78ba4123bbccbefbba96bad6
esperoj <esperoj@esperoj.eu.org>
2023-12-21 16:57:40 +0000

    add test

--- tests/test_database.py
+++ tests/test_database.py
@@ -1,108 +1,102 @@
-"""Tests for database table functionality in Esperoj."""
+from collections.abc import Iterable
+from uuid import UUID
 
 import pytest
 
-from esperoj.exceptions import InvalidRecordError, RecordNotFoundError
+from esperoj.database.memory import MemoryDatabase, MemoryRecord, MemoryTable
+from esperoj.exceptions import RecordNotFoundError
 
 
 @pytest.fixture()
-def record(table):
-    """Create a test record in the table."""
-    return table.create({"name": "test"})
+def db():
+    return MemoryDatabase("test_db")
 
+@pytest.fixture()
+def table(db):
+    return db.table("test_table")
 
-def test_database_table_creation(db):
-    """Test creating a table using the database instance."""
-    table = db.table("test_table")
+def test_memory_database_creation(db):
+    assert isinstance(db, MemoryDatabase)
+    assert db.name == "test_db"
+
+def test_memory_table_creation(table):
+    assert isinstance(table, MemoryTable)
     assert table.name == "test_table"
 
+@pytest.mark.parametrize("fields", [
+    {"name": "John", "age": 30},
+    {"name": "Jane", "age": 28, "city": "New York"},
+])
+def test_memory_record_creation(table, fields):
+    record = table.create(fields)
+    assert isinstance(record, MemoryRecord)
+    assert isinstance(record.record_id, UUID)
+    assert record.fields == fields
 
-def test_database_table_creation_error(db):
-    """Test error handling for invalid table names."""
-    with pytest.raises(ValueError, match="Table name must be a string."):
-        db.table(123)
+@pytest.mark.parametrize("fields, update_fields, expected_fields", [
+    ({"name": "John", "age": 30}, {"age": 31}, {"name": "John", "age": 31}),
+    ({"name": "Jane", "age": 28, "city": "New York"}, {"city": "Los Angeles"}, {"name": "Jane", "age": 28, "city": "Los Angeles"}),
+])
+def test_memory_record_update(table, fields, update_fields, expected_fields):
+    record = table.create(fields)
+    updated_record = record.update(update_fields)
+    assert isinstance(updated_record, MemoryRecord)
+    assert updated_record.fields == expected_fields
 
+def test_memory_table_get(table):
+    fields = {"name": "John", "age": 30}
+    record = table.create(fields)
+    fetched_record = table.get(record.record_id)
+    assert fetched_record == record
 
-def test_table_create(table):
-    """Test creating a record in a table."""
-    record = table.create({"name": "test"})
-    assert "id" in record
-    assert record["fields"] == {"name": "test"}
-
-
-@pytest.mark.parametrize("invalid_input", [123, "string", None])
-def test_table_create_error(table, invalid_input):
-    """Test error handling for invalid record data."""
-    with pytest.raises(InvalidRecordError):
-        table.create(invalid_input)
-
-
-def test_table_delete(table, record):
-    """Test deleting a record from a table."""
-    record_id = record["id"]
-    assert table.delete(record_id) == record_id
-
-
-def test_table_delete_error(table):
-    """Test error handling for deleting a non-existent record."""
+def test_memory_table_get_not_found(table):
     with pytest.raises(RecordNotFoundError):
-        table.delete("non_existent_id")
+        table.get(UUID(int=0))
 
-
-def test_table_get(table, record):
-    """Test retrieving a record by ID."""
-    record_id = record["id"]
-    assert table.get(record_id) == record
-
-
-def test_table_get_error(table):
-    """Test error handling for retrieving a non-existent record."""
+def test_memory_table_delete(table):
+    fields = {"name": "John", "age": 30}
+    record = table.create(fields)
+    deleted_id = table.delete(record.record_id)
+    assert deleted_id == record.record_id
     with pytest.raises(RecordNotFoundError):
-        table.get("non_existent_id")
+        table.get(record.record_id)
 
-
-def test_table_get_all(table, record):
-    """Test retrieving all records from a table."""
-    assert table.get_all() == [record]
-
-
-def test_table_match(table, record):
-    """Test retrieving records matching specific criteria."""
-    assert table.match({"name": "test"}) == [record]
-
-
-def test_table_update(table, record):
-    """Test updating an existing record."""
-    updated_record = table.update({"id": record["id"], "fields": {"name": "updated"}})
-    assert updated_record["fields"]["name"] == "updated"
-
-
-def test_table_update_error(table):
-    """Test error handling for updating a non-existent record."""
+def test_memory_table_delete_not_found(table):
     with pytest.raises(RecordNotFoundError):
-        table.update({"id": "non_existent_id", "fields": {"name": "updated"}})
+        table.delete(UUID(int=0))
 
+@pytest.mark.parametrize("records, formulas, expected_records", [
+    ([{"name": "John", "age": 30}, {"name": "Jane", "age": 28}], {"age": 30}, [{"name": "John", "age": 30}]),
+    ([{"name": "John", "age": 30}, {"name": "Jane", "age": 28}], {"age": 40}, []),
+])
+def test_memory_table_get_all(table, records, formulas, expected_records):
+    created_records = [table.create(record) for record in records]
+    fetched_records = list(table.get_all(formulas))
+    assert len(fetched_records) == len(expected_records)
+    for record in fetched_records:
+        assert record.fields in expected_records
 
-def test_base_table_create_many(table):
-    """Test creating multiple records in a single call."""
-    count = 2
-    records = table.create_many([{"name": "test1"}, {"name": "test2"}])
-    assert len(records) == count
+def test_memory_table_update(table):
+    fields = {"name": "John", "age": 30}
+    update_fields = {"age": 31}
+    record = table.create(fields)
+    updated_record = table.update(record.record_id, update_fields)
+    assert isinstance(updated_record, MemoryRecord)
+    assert updated_record.fields == {**fields, **update_fields}
 
+def test_memory_table_update_not_found(table):
+    with pytest.raises(RecordNotFoundError):
+        table.update(UUID(int=0), {"age": 31})
 
-def test_base_table_delete_many(table, record):
-    """Test deleting multiple records by ID."""
-    record_id = record["id"]
-    assert table.delete_many([record_id]) == [record_id]
+def test_memory_database_table(db):
+    table1 = db.table("table1")
+    table2 = db.table("table2")
+    assert table1 != table2
+    assert table1.name == "table1"
+    assert table2.name == "table2"
 
-
-def test_base_table_get_many(table, record):
-    """Test retrieving multiple records by ID."""
-    record_id = record["id"]
-    assert table.get_many([record_id]) == [record]
-
-
-def test_base_table_update_many(table, record):
-    """Test updating multiple records in a single call."""
-    updated_records = table.update_many([{"id": record["id"], "fields": {"name": "updated"}}])
-    assert updated_records[0]["fields"]["name"] == "updated"
+def test_memory_database_close(db):
+    table = db.table("test_table")
+    table.create({"name": "John", "age": 30})
+    assert db.close()
+    assert not db.tables
