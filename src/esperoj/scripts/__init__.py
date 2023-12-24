4d7b4dcc0dfdf53a55b6fc37e29a4a3e9a2f7988
esperoj <esperoj@esperoj.eu.org>
2023-12-24 12:51:42 +0000

    Add verify script

--- src/esperoj/scripts/__init__.py
+++ src/esperoj/scripts/__init__.py
@@ -1,4 +1,5 @@
 """Module to run scripts."""
+import datetime
 import time
 
 import typer
@@ -7,6 +8,10 @@ from esperoj import Esperoj
 
 app = typer.Typer()
 Context = typer.Context
+
+
+class VerifyError(Exception):
+    """Error when file integrity is compromised."""
 
 
 def get_obj(ctx: Context) -> Esperoj:
@@ -36,6 +41,28 @@ def archive(ctx: Context) -> None:
         print(f"Start to archive file `{name}`")
         es.archive(file.record_id)
         print(f"Finish file `{name}` in {time.time() - start} second")
+
+
+@app.command()
+def verify(ctx: Context) -> None:
+    """Archives all files that have not been archived yet.
+
+    Args:
+        ctx: The Typer context.
+    """
+    es = get_obj(ctx)
+    files = list(es.db.table("Files").get_all(sort=["-Created"]))
+    num_shards = 7
+    shard_size = len(files) // num_shards
+    today = datetime.datetime.now(datetime.UTC).weekday()
+    for i in range(shard_size):
+        file = files[today * shard_size + i]
+        name = file.fields["Name"]
+        print(f"Start to verify file `{name}`")
+        start = time.time()
+        if es.verify(file.record_id) is not True:
+            raise VerifyError(f"Failed to verify {name}")
+        print(f"Verified file `{name}` in {time.time() - start} second")
 
 
 if __name__ == "__main__":
