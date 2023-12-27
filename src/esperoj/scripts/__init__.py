5c14af1fb8391a564739ca1765ce33b26b31a580
esperoj <esperoj@esperoj.eu.org>
2023-12-27 07:27:11 +0000

    Run 4 verify jobs at the same time

--- src/esperoj/scripts/__init__.py
+++ src/esperoj/scripts/__init__.py
@@ -1,4 +1,5 @@
 """Module to run scripts."""
+import concurrent.futures
 import datetime
 import time
 
@@ -27,23 +28,6 @@ def get_obj(ctx: Context) -> Esperoj:
 
 
 @app.command()
-def archive(ctx: Context) -> None:
-    """Archives all files that have not been archived yet.
-
-    Args:
-        ctx: The Typer context.
-    """
-    es = get_obj(ctx)
-    files = es.db.table("Files").get_all({"Internet Archive": ""})
-    for file in files:
-        start = time.time()
-        name = file.fields["Name"]
-        print(f"Start to archive file `{name}`")
-        es.archive(file.record_id)
-        print(f"Finish file `{name}` in {time.time() - start} second")
-
-
-@app.command()
 def verify(ctx: Context) -> None:
     """Verifies the integrity of files by weekday.
 
@@ -55,7 +39,8 @@ def verify(ctx: Context) -> None:
     num_shards = 7
     shard_size = len(files) // num_shards
     today = datetime.datetime.now(datetime.UTC).weekday()
-    for i in range(shard_size):
+
+    def verify_file(i):
         file = files[today * shard_size + i]
         name = file.fields["Name"]
         print(f"Start to verify file `{name}`")
@@ -63,6 +48,9 @@ def verify(ctx: Context) -> None:
         if es.verify(file.record_id) is not True:
             raise VerifyError(f"Failed to verify {name}")
         print(f"Verified file `{name}` in {time.time() - start} second")
+
+    with concurrent.futures.ThreadPoolExecutor(max_workers=4) as executor:
+        executor.map(verify_file, range(shard_size))
 
 
 if __name__ == "__main__":
