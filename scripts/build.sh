bf64b5a4c1692bc3875abcc233b26d3d89cad1d6
esperoj <esperoj@esperoj.eu.org>
2024-03-10 14:28:08 +0100

    add docs

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-
+uname -a
+uptime
+lscpu
+df -hT
+apt update -yqq
+apt install -yqq curl wget
+curl -sL yabs.sh | bash -s -- -s https://www.vpsbenchmarks.com/yabs/upload
 pip install poetry
 poetry install --with test,dev
 poetry run poe docs
