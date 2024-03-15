6aa85af3fab3729b860a1077706bc553184d5875
esperoj <esperoj@esperoj.eu.org>
2024-03-15 05:31:18 +0000

    refactory database.py

--- src/esperoj/database/seatable.py
+++ src/esperoj/database/seatable.py
@@ -3,7 +3,7 @@ import os
 import uuid
 from collections.abc import Iterator
 from typing import Any, Self
-
+from operator import itemgetter
 from seatable_api import Base, context
 
 from esperoj.database.database import (
@@ -35,14 +35,24 @@ class SeatableTable(Table):
             if key.startswith("_"):
                 record_dict.pop(key)
             if key in self.links:
-                record_dict[key] = [item["row_id"] for item in record_dict[key]]
+                record_dict[key] = [item["row_id"] if isinstance(item, dict) else item for item in record_dict[key]]
         return SeatableRecord(record_id=record_id, fields=record_dict, table=self)
+
+    def _update_links_from_fields_list(self, fields_list: list[Fields]) -> bool:
+        links = {key: {} for key in self.links}
+        for fields in fields_list:
+            for key, value in fields.items():
+                if key in links:
+                    links[key][fields["_id"]] = value
+        return all(self.batch_update_links(key, value) for key, value in links.items() if value != {})
 
     def batch_create(self, fields_list: list[Fields]) -> list[Record]:
         # BUG: The batch_delete yield error for different id format.
         fields_list = [{"_id": str(uuid.uuid4())[:22]} | fields for fields in fields_list]
         if self.client.batch_append_rows(self.name, fields_list)["inserted_row_count"] is not len(fields_list):
             raise RuntimeError("Failed to create all rows")
+        if not self._update_links_from_fields_list(fields_list):
+            raise RuntimeError("Failed to link all records")
         return [self._record_from_dict(fields) for fields in fields_list]
 
     def batch_delete(self, record_ids: list[RecordId]) -> list[RecordId]:
@@ -59,6 +69,7 @@ class SeatableTable(Table):
 
     def batch_get_link_id(self, field_keys: list[FieldKey]) -> dict[FieldKey, str]:
         """Get the link ids for the given field keys."""
+        return {key : self.links[key]["link_id"] for key in field_keys}
 
     def batch_update(self, records: list[tuple[RecordId, Fields]]) -> dict[RecordId, Record]:
         """Update the records with the given record_ids with the given fields."""
@@ -66,10 +77,12 @@ class SeatableTable(Table):
     def batch_update_links(
         self,
         field_key: FieldKey,
-        other_table_name: str,
         record_ids_map: dict[RecordId, list[RecordId]],
-    ):
+    ) -> bool:
         """Update the links between the records with the given record_ids and the records with the given other_record_ids in the other table."""
+        link_id = self.get_link_id(field_key)
+        other_table_id = self.links[field_key]["other_table_id"]
+        return self.client.batch_update_links(link_id, self.name, other_table_id, list(record_ids_map.keys()), record_ids_map)["success"]
 
     def get_linked_records(
         self, field_key: FieldKey, record_ids: list[RecordId]
