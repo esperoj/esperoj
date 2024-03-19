f2c698db28bf7a34a874330ea86210c4d4be7e37
esperoj <esperoj@esperoj.eu.org>
2024-03-19 04:19:10 +0000

    backup metadata.json too

--- src/esperoj/scripts/export_database.py
+++ src/esperoj/scripts/export_database.py
@@ -4,11 +4,14 @@ from functools import partial
 
 def export_database(esperoj, name):
     db = esperoj.databases[name]
-    for table in db.metadata["tables"]:
+    metadata = db.metadata
+    for table in metadata["tables"]:
         table_name = table["name"]
         data = [record.to_dict() for record in db.get_table(table_name).query("$[*]")]
         with open(f"{table_name}.json", "w") as f:
             json.dump(data, f)
+    with open("Metadata.json", "w") as f:
+        json.dump(metadata, f)
 
 
 def get_esperoj_method(esperoj):
