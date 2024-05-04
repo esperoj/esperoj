ad7679cc4c3c7a33498c72821a6479f6bf2fef68
esperoj <esperoj@esperoj.eu.org>
2024-05-04 10:36:42 +0000

    we can ingest folder now

--- src/esperoj/scripts/ingest.py
+++ src/esperoj/scripts/ingest.py
@@ -2,65 +2,101 @@ from esperoj.database.database import Record
 from esperoj.utils import calculate_hash
 from pathlib import Path
 from functools import partial
+from concurrent.futures import ProcessPoolExecutor, as_completed
+import json
+import subprocess
 
 
-def ingest(esperoj, path: Path) -> Record:
+def ingest(esperoj, path: Path) -> list[Record]:
     """Ingest a file into the Esperoj system.
 
     Args:
         esperoj (object): The Esperoj object representing the system.
-        path (Path): The path to the file to be ingested.
+        path (Path): The path to be ingested.
 
     Returns:
-        Record: The database record representing the ingested file.
+        list(Record): The database records representing the ingested files.
 
     Raises:
         FileNotFoundError: If the specified file does not exist.
         FileExistsError: If the file already exists in the system.
         RuntimeError: If the file type is not supported.
     """
-    if not path.is_file():
-        raise FileNotFoundError
+    logger = esperoj.loggers["Primary"]
 
-    name = path.name
-    size = path.stat().st_size
-    f = path.open("rb")
-    sha256sum = calculate_hash(f, algorithm="sha256")
-    f.close()
-    files = esperoj.databases["Primary"].get_table("Files")
+    def ingest_file(file_path: Path) -> Record:
+        logger.info(f"Start to ingest file `{str(file_path)}`")
 
-    def add(storage_name):
-        """Add the file to the specified storage and create a database record for it.
+        name = file_path.name
+        size = file_path.stat().st_size
+        f = file_path.open("rb")
+        sha256sum = calculate_hash(f, algorithm="sha256")
+        f.close()
+        metadata = json.loads(subprocess.check_output(["exiftool", "-j", str(file_path)]))[0]
+        files = esperoj.databases["Primary"].get_table("Files")
+        musics = esperoj.databases["Primary"].get_table("Musics")
 
-        Args:
-            storage_name (str): The name of the storage where the file should be stored.
+        def upload(storage_names: list[str]) -> Record:
+            """Upload the file to the specified storages and create a database record for it.
 
-        Returns:
-            Record: The database record representing the ingested file.
+            Args:
+                storage_names (list[str]): The names of the storages where the file should be stored.
 
-        Raises:
-            FileExistsError: If the file already exists in the storage or database.
-        """
-        storage = esperoj.storages[storage_name]
-        if storage.file_exists(name) or files.query('$[?Name = "{name}"]') != []:
-            raise FileExistsError
-        storage.upload_file(str(path), name)
-        return files.create(
-            {
-                "Name": name,
-                "Size": size,
-                "SHA256": sha256sum,
-                "Internet Archive": "https://example.com/",
-                "Storage": storage_name,
-                "Metadata": "To be added",
-            }
-        )
+            Returns:
+                Record: The database record representing the ingested file.
 
-    match path.suffix:
-        case ".flac" | ".mp3" | ".m4a":
-            return add("Audio Storage")
-        case _:
-            raise RuntimeError("File type is not supported.")
+            Raises:
+                FileExistsError: If the file already exists in any of the storages or database.
+            """
+            if files.query('$[?Name = "{name}"]') != []:
+                raise FileExistsError
+            for storage_name in storage_names:
+                storage = esperoj.storages[storage_name]
+                if storage.file_exists(name):
+                    raise FileExistsError
+                storage.upload_file(str(file_path), name)
+
+            return files.create(
+                {
+                    "Name": name,
+                    "Size": size,
+                    "SHA256": sha256sum,
+                    "Internet Archive": "https://example.com/",
+                    "Storages": json.dumps(storage_names),
+                    "Metadata": json.dumps(metadata),
+                }
+            )
+
+        match path.suffix:
+            case ".flac" | ".mp3" | ".m4a":
+                file_id = upload(["Audio Storage", "Backup Audio Storage"]).record_id
+                return musics.create(
+                    {"Title": metadata["Title"], "Artist": metadata["Artist"], "Files": [file_id]}
+                )
+            case _:
+                raise RuntimeError("File type is not supported.")
+
+    file_paths = []
+
+    if path.is_dir():
+        file_paths = [file_path for file_path in path.iterdir() if file_path.is_file()]
+    else:
+        if not path.is_file():
+            raise FileNotFoundError
+        file_paths = [path]
+
+    with ProcessPoolExecutor(max_workers=4) as executor:
+        results = []
+        future_to_file_path = {
+            executor.submit(ingest_file, file_path): file_path for file_path in file_paths
+        }
+        for future in as_completed(future_to_file_path):
+            try:
+                results.append(future.result())
+                logger.info(f"Successful ingested file `{future_to_file_path[future]}`")
+            except Exception as e:
+                logger.error(str(e))
+        return results
 
 
 def get_esperoj_method(esperoj):
@@ -85,16 +121,16 @@ def get_click_command():
 
     @click.command()
     @click.argument(
-        "file_path", type=click.Path(exists=True, dir_okay=False, path_type=Path), required=True
+        "file_path", type=click.Path(exists=True, dir_okay=True, path_type=Path), required=True
     )
     @click.pass_obj
-    def click_command(esperoj, file_path: Path):
-        """Ingest a file into the Esperoj system.
+    def click_command(esperoj, path: Path):
+        """Ingest a file or a folder into the Esperoj system.
 
         Args:
             esperoj (object): The Esperoj object representing the system.
-            file_path (Path): The path to the file to be ingested.
+            path (Path): The path to be ingested.
         """
-        print(ingest(esperoj, file_path))
+        print(ingest(esperoj, path))
 
     return click_command
