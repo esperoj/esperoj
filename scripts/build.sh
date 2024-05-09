39fe8862632cca04a0c418982e85e6ef5b71ebdd
esperoj <esperoj@esperoj.eu.org>
2024-05-09 09:59:36 +0000

    update

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-
-pip install poetry
 poetry install --with test,dev
 poetry run poe docs
+rclone copy -v pcloud:workspace/backup backup
+7zz a "-p${ENCRYPTION_PASSPHRASE}" backup.7z backup
+rm -r backup
+mkdir public
+mv backup.7z docs public
+cd public
+echo "/backup $(pomfload backup.7z)" > _redirects
