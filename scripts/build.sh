1f945b0c46655d86bb8b6856ad22d9d80875662d
esperoj <esperoj@esperoj.eu.org>
2024-05-09 10:55:57 +0000

    update

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 poetry install --with test,dev
 poetry run poe docs
-rclone copy -v pcloud:workspace/backup backup
-7zz a "-p${ENCRYPTION_PASSPHRASE}" backup.7z backup
-rm -r backup
-mkdir public
+wget --no-verbose "https://public.esperoj.eu.org/backup.7z"
 mv backup.7z docs public
 cd public
-echo "/backup $(pomfload backup.7z)" > _redirects
+url="$(curl -sL https://raw.githubusercontent.com/esperoj/dotfiles/main/bin/pomfload | sh -s -- backup.7z)"
+echo "/backup ${url}" > _redirects
