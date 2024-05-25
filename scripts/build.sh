def172bee02740c0e02e26d4125e758ee028fde4
esperoj <esperoj@esperoj.eu.org>
2024-05-25 11:23:18 +0000

    test

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-info.sh
+
 set -Exeo pipefail
+
 python -m venv .venv
 . ./.venv/bin/activate
 poetry install --with test,dev
 poetry run poe docs
-rclone sync workspace:backup ./backup
+rclone sync -v workspace:backup ./backup
 7zz a "-p${ENCRYPTION_PASSPHRASE}" backup.7z ./backup
 mv backup.7z docs public
