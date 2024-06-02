2bb78f6aea48b2fa59c09e9277c0f6cbac39f658
esperoj <esperoj@esperoj.eu.org>
2024-06-02 15:21:57 +0000

    update

--- projects/esperoj/esperoj/__main__.py
+++ projects/esperoj/esperoj/__main__.py
@@ -2,15 +2,5 @@
 
 from esperoj.cli import cli
 
-
-def show_modules():
-    """
-    This function let Nuitka see modules that need to be imported.
-    """
-    import esperoj.utils
-
-    print(esperoj.utils)
-
-
 if __name__ == "__main__":
     cli()
