47ae6f561db7dac1958896e45efd466e51e60ee6
esperoj <esperoj@esperoj.eu.org>
2024-06-07 02:58:14 +0000

    update

--- projects/esperoj/esperoj/server.py
+++ projects/esperoj/esperoj/server.py
@@ -1,13 +1,8 @@
 """Server module."""
 
-from pathlib import Path
-
 import requests
 from litestar import Litestar, get
 from litestar.response import Stream
-from litestar.static_files import create_static_files_router
-
-PUBLIC_DIR = Path("public")
 
 
 @get("/backup.7z", media_type="application/x-7z-compressed")
@@ -17,21 +12,4 @@ async def get_backup() -> Stream:
     return Stream(response.iter_content(2**20))
 
 
-@get("/api/pwd")
-async def pwd() -> list[str]:
-    cwd = Path.cwd()
-    return [item.name for item in cwd.iterdir()]
-
-
-def on_startup():
-    PUBLIC_DIR.mkdir(exist_ok=True)
-
-
-app = Litestar(
-    route_handlers=[
-        pwd,
-        get_backup,
-        create_static_files_router(path="/", directories=[str(PUBLIC_DIR)], html_mode=True),
-    ],
-    on_startup=[on_startup],
-)
+app = Litestar(route_handlers=[get_backup])
